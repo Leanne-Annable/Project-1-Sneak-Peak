05a90c95c8adcf7cc2be237c69e68a6f6c5918f1
Litttle-panda <117175961+Litttle-panda@users.noreply.github.com>
2023-02-02 13:10:35 +0000

    added pesudo code to js file

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+/* Create an API call on page load to search for movies released within the last 2 months
+    using the "released" or "Year" key from the OMDB info.
+    Will probably need to use moment.js to set a time scale? 
+    Or we could find another type of movies to link to the page - Maybe "classics" instead of recent releases
+    if the code becomes too challenging within our timescale
+
+
+/* create an API to call information from OMDB containing 
+    -   The Movie Title
+    -   Age Rating
+    -   Movie Length
+    -   The Plot
+    -   The Main Actors
+    -   The Release date
+    -   Genre
+    -   The IMDB Rating
+    -   The movie poster
+
+
+    Will need the API search to be given by either the inputted text, or the clicked image
+
+
+/* create an API to YouTube to get the movie trailer 
+    (This again will need to be linked to either the inputted text or clicked image)
+
+    in the call code we will need to add "movie trailer" so the youtube search looks for that specifically
+    
+*/
