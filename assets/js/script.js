8f6782769ef6d86fceca3cc9d12c870e8443c848
Litttle-panda <117175961+Litttle-panda@users.noreply.github.com>
2023-02-02 16:47:41 +0000

    Got the search button to run the API call to OMDB

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,13 +32,14 @@ var searchInput = $("#search-input"); // the text box
 var searchButton = $("#search-button"); // the search button
 var movieStatics = $("#movie-statics"); // the div section where the movie suggestions will be held
 var movieSuggestions = ["Avatar: The Way of Water", "Black Panther: Wakanda Forever", "Minions: The Rise of Gru", "Sonic The HedgeHog 2"];
+// list of movies to show on screen - will need to add moreto the list and add a random feature to select different ones each time for variety
+var movieInput = ""
 
 displaySearch()
 // functions
 
 function displaySearch() {
     // search through the movie list to display each icon
-
     for (var i = 0; i < movieSuggestions.length; i++) {
         var movie = movieSuggestions[i]
         var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy"
@@ -61,7 +62,73 @@ function displaySearch() {
     }
 }
 
+function getMovieInfo(){
+    // get the movie typed in the search bar
+    var movie = $("#search-input").val();
+    var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy"
+    // ajax call on the searched info
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+
+        // Creating a div to hold the movie
+        var movieDiv = $("<div class='movie'>");
+
+        // Storing the rating data
+        var rating = response.Rated;
+
+        // Creating an element to have the rating displayed
+        var pOne = $("<p>").text("Rating: " + rating);
+
+        // Displaying the rating
+        movieDiv.append(pOne);
+
+        // Storing the release year
+        var released = response.Released;
+
+        // Creating an element to hold the release year
+        var pTwo = $("<p>").text("Released: " + released);
+
+        // Displaying the release year
+        movieDiv.append(pTwo);
+
+        // Storing the plot
+        var plot = response.Plot;
+
+        // Creating an element to hold the plot
+        var pThree = $("<p>").text("Plot: " + plot);
+
+        // Appending the plot
+        movieDiv.append(pThree);
+
+        // Retrieving the URL for the image
+        var imgURL = response.Poster;
+
+        // Creating an element to hold the image
+        var image = $("<img>").attr("src", imgURL);
+
+        // Appending the image
+        movieDiv.append(image);
+
+        // Putting the entire movie above the previous movies
+        $("#movie-info").append(movieDiv);
+    });
+}
 
 
 
 // click events
+
+// search button click event
+$("#search-button").on("click", function(event){
+    event.preventDefault();
+    movieInput = searchInput.val();
+
+    if (movieInput === "" || movieInput === " "){
+        alert("Please enter a movie to search for")
+        return
+    }
+
+    getMovieInfo()
+})
