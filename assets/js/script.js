2f07911085dc0f6ed16cbced80a37dceea774360
Litttle-panda <117175961+Litttle-panda@users.noreply.github.com>
2023-02-03 15:28:11 +0000

    switching branches

--- assets/js/script.js
+++ assets/js/script.js
@@ -69,7 +69,10 @@ function displaySearch() {
             method: "GET",
         }).then(function (response) {
             // Creating a div to hold the movie
-            var movieDiv = $("<div class='moviePull col-md-3'>");
+            var movieDiv = $("<div class='moviePull col-md-3' id='moviePoster'>");
+            // store the movies name
+            movieDiv.attr("data-name", movieSuggestions[i])
+            console.log(movieSuggestions[i])
             // Retrieving the URL for the image
             var imgURL = response.Poster;
             // Creating an element to hold the image
@@ -77,7 +80,7 @@ function displaySearch() {
             // Appending the image
             movieDiv.append(image);
             // append to screen
-            movieStatics.append(movieDiv)
+            movieStatics.append(movieDiv);
         })
     }
 }
@@ -137,7 +140,62 @@ function getMovieInfo() {
     });
 }
 
+function displayMovieInfo() {
 
+    // var movie = $(event.target).attr("data-name");
+    var movie = $(this).attr("data-name");
+    var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
+  
+     // Creating an AJAX call for the specific movie button being clicked
+     $.ajax({
+       url: queryURL,
+       method: "GET"
+     }).then(function(response) {
+  
+       // Creating a div to hold the movie
+       var movieDiv = $("<div class='movie'>");
+  
+       // Storing the rating data
+       var rating = response.Rated;
+  
+       // Creating an element to have the rating displayed
+       var pOne = $("<p>").text("Rating: " + rating);
+  
+       // Displaying the rating
+       movieDiv.append(pOne);
+  
+       // Storing the release year
+       var released = response.Released;
+  
+       // Creating an element to hold the release year
+       var pTwo = $("<p>").text("Released: " + released);
+  
+       // Displaying the release year
+       movieDiv.append(pTwo);
+  
+       // Storing the plot
+       var plot = response.Plot;
+  
+       // Creating an element to hold the plot
+       var pThree = $("<p>").text("Plot: " + plot);
+  
+       // Appending the plot
+       movieDiv.append(pThree);
+  
+       // Retrieving the URL for the image
+       var imgURL = response.Poster;
+  
+       // Creating an element to hold the image
+       var image = $("<img>").attr("src", imgURL);
+  
+       // Appending the image
+       movieDiv.append(image);
+  
+       // Putting the entire movie above the previous movies
+       $("#movies-view").prepend(movieDiv);
+     });
+  
+   }
 
 // click events
 
@@ -155,3 +213,4 @@ $("#search-button").on("click", function (event) {
     searchInput.val("")
 })
 
+$("#moviePoster").on("click", ".moviePull", displayMovieInfo)
