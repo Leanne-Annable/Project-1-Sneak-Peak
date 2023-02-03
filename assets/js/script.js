6396cdb13b663476b590315832a9bea2f0f02d2c
Litttle-panda <117175961+Litttle-panda@users.noreply.github.com>
2023-02-03 13:30:24 +0000

    Added the fisher-yates random sort function to shuffle the movies displayed on screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,17 +31,37 @@
 var searchInput = $("#search-input"); // the text box
 var searchButton = $("#search-button"); // the search button
 var movieStatics = $("#movie-statics"); // the div section where the movie suggestions will be held
-var movieSuggestions = ["Avatar: The Way of Water", "Black Panther: Wakanda Forever", "Minions: The Rise of Gru", "Sonic The HedgeHog 2"];
+var movieSuggestions = ["Avatar: The Way of Water", "Black Panther: Wakanda Forever", "Minions: The Rise of Gru", "Sonic The HedgeHog 2",
+    "Bros", "You People", "Jung_E", "The Last Manhunt", "The Invitation", "Everything Everywhere all at Once", "smile", "Bullet Train",
+    "The Lost City", "Lightyear", "Uncharted", "Morbius", "Turning Red", "Doctor Strange and the Multiverse of Madness", "Black Adam",
+    "Thor: Love and Thunder", "The Bad Guys", "Strange World", "Jurassic World Dominion", "Luck", "The Sea Beast", "Slumberland", "Chip 'n Dale: Rescue Rangers"];
+
+// var moviesRandom = movieSuggestions[Math.floor(Math.random()*movieSuggestions.length)]
 // list of movies to show on screen - will need to add moreto the list and add a random feature to select different ones each time for variety
 var movieInput = ""
 
 displaySearch()
 // functions
+// Fisher-Yates (aka Knuth) Shuffle method
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+    }
+    return array;
+  }
+
 
 function displaySearch() {
+    shuffle(movieSuggestions)
     // search through the movie list to display each icon
-    for (var i = 0; i < movieSuggestions.length; i++) {
-        var movie = movieSuggestions[i]
+    for (var i = 0; i < 9; i++) {
+        var movie = movieSuggestions[i];
         var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy"
 
         $.ajax({
@@ -62,7 +82,7 @@ function displaySearch() {
     }
 }
 
-function getMovieInfo(){
+function getMovieInfo() {
     $("#movie-info").empty()
     // get the movie typed in the search bar
     var movie = $("#search-input").val();
@@ -71,7 +91,7 @@ function getMovieInfo(){
     $.ajax({
         url: queryURL,
         method: "GET"
-    }).then(function(response) {
+    }).then(function (response) {
 
         // Creating a div to hold the movie
         var movieDiv = $("<div class='movie'>");
@@ -122,11 +142,11 @@ function getMovieInfo(){
 // click events
 
 // search button click event
-$("#search-button").on("click", function(event){
+$("#search-button").on("click", function (event) {
     event.preventDefault();
     movieInput = searchInput.val();
 
-    if (movieInput === "" || movieInput === " "){
+    if (movieInput === "" || movieInput === " ") {
         alert("Please enter a movie to search for")
         return
     }
